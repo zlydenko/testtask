f349dd4a7cd81f82e1cfa4d6e5eaf5e10729154b
anton zlydenko <anton.zlydenko@gmail.com>
2018-08-01 11:13:34 +0300

    move from gulp to webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+const devMode = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  entry: "./src/js/app.js",
+  module: {
+    rules: [
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: [
+                autoprefixer({
+                  browsers: ["ie >= 8", "last 4 versions"]
+                })
+              ]
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new MiniCssExtractPlugin({
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "../index.html"
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  }
+};
